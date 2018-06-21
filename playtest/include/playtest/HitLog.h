42c88f69e1cae4079b0e7090d5561d4803198077
Tom Eichlersmith <eichl008@umn.edu>
2018-06-21 09:47:43 -0500

    migrated some functions to HitLog class in separate file

---
+++ playtest/include/playtest/HitLog.h
@@ -0,0 +1,86 @@
+/**
+ * @file HitLog.h
+ * @brief Class implementation of hit log to search and reconstruct.
+ * @author Tom Eichlersmith, University of Minnesota
+ */
+
+#ifndef PLAYTEST_HITLOG_H
+#define PLAYTEST_HITLOG_H
+
+//Standard Libraries
+#include <iostream> //Checks to std::cout for development purposes
+#include <vector> //Vector of hits per layer
+#include <map> //std::map for storage tree
+#include <utility> //std::pair for storage tree
+#include <iterator> //std::next and std::prev for search through map
+#include <cmath> //floor and ceil for stripbounds calculations
+
+//LDMX Framework
+#include "Event/HcalHit.h" //Get Hcal Specific information from hit
+
+
+namespace ldmx {
+
+    //* type that will be used to reference hits
+    typedef const ldmx::HcalHit* HitPtr;
+
+    /**
+     * @class HitLog
+     * @brief Stores HitPtrs in a std::map for easy searching and track reconstruction.
+     */
+    class HitLog {
+        public:
+
+        private:
+ 
+            float minPE_; //* Minimum number of PEs to not be considered noise
+
+            int conedepth_; //* depth of search cone around seed in layers
+
+            float coneangle_; //* angular opening of cone around seed in strips across the first layer
+
+            float origin_; //* center of Ecal in strips
+            float lowside_; //* low side of Ecal in strips
+            float upside_; //* upper side of Ecal in strips
+
+             /**
+             * Function to generate key for a given hit.
+             * Relies on layermod_.
+             *
+             * @param hit pointer to HcalHit instance that a key is needed for
+             * @return integer key value
+             */
+            int keygen( HitPtr hit ) const;
+
+             /**
+             * Function to search a specific range of a HitLog for a hit.
+             * Returns a pair of nullptrs if failed to find an isolated hit.
+             *
+             * @param log HitLog to be searched
+             * @param lowkey lower bound key
+             * @param upkey upper bound key
+             * @return HitPtr pair to isolated hit (single strip or two strip combo)
+             */
+            std::pair< HitPtr , HitPtr > search( const HitLog log , const int lowkey , const int upkey ) const;
+
+            /**
+             * Function to find strip bounds for input layer given seed layer.
+             * This function assumes that the possible track originates from the Ecal.
+             * Within a few layers of the seed, this function will do a good job shrinking the parameter space.
+             * Farther away from the seed (in layers), this function will only work if the track did originate
+             *  from the Ecal.
+             *
+             * @param seedlayer layer number of seed
+             * @param seedstrip strip number of seed
+             * @param layer layer number of interest
+             * @param lowstrip strip number of lower bound
+             * @param upstrip strip number of upper bound
+             * @return true if projected range intersects the input layer
+             */
+            bool stripbounds( const int seedlayer , const int seedstrip , const int layer , int &lowstrip , int &upstrip ) const;
+
+    };
+
+}
+
+#endif /* PLAYTEST_HITLOG_H */
