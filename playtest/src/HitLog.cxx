96e9c9fdeb4324c1ec8e1470bbdd13534e6be2a1
Tom Eichlersmith <eichl008@umn.edu>
2018-06-21 13:27:15 -0500

    first draft of SearchLayer function

--- playtest/src/HitLog.cxx
+++ playtest/src/HitLog.cxx
@@ -32,6 +32,17 @@ namespace ldmx {
         return static_cast<int>( hit->getLayer()*layermod_ + hit->getStrip() );
     }
     
+    void CorrectStrip( int &strip ) const {
+        
+        if ( strip < 0 ) {
+            strip = 0;
+        } else if ( strip > nstrips_ ) {
+            strip = nstrips_;
+        }
+        
+        return;
+    }
+    
     bool HitLog::FindSeed( int &seedlayer , int &seedstrip ) const {
 
     }
@@ -45,11 +56,56 @@ namespace ldmx {
     }
             
     bool HitLog::SearchLayer( const int layer , std::vector< HitPtr > &track ) const {
+        //Find leftmost, secondleftmost, rightmost, secondrightmost (left and right sides could be equal)
+        std::pair< HitPtr , HitPtr > leftmost( track[0], track[1] ), rightmost( track[0] , track[1] );
+        for ( std::vector< HitPtr >::iterator it = track.begin(); it != track.end(); ++it ) {
+            
+            float curr_strip = (*it)->getStrip();
+            
+            //Check if curr_strip is first or second most left
+            if ( curr_strip < leftmost.first->getStrip() ) {
+                leftmost.second = leftmost.first;
+                leftmost.first = (*it);
+            } else if ( curr_strip < leftmost.second->getStrip() ) {
+                leftmost.second = (*it);
+            }
+            
+            //Check if curr_strip is first or second most right
+            if ( curr_strip > rightmost.first->getStrip() ) {
+                rightmost.second = rightmost.first;
+                rightmost.first = (*it);
+            } else if ( curr_strip > rightmost.second->getStrip() ) {
+                rightmost.second = (*it);
+            }
+        } //iterate through partial track (it)
+        
+        //Extend from leftmost to layer
+        float leftslope = (leftmost.first->getStrip() - leftmost.second->getStrip())/(leftmost.first->getLayer() - leftmost.second->getLayer());
+        float leftedge = (layer - leftmost.first->getLayer())*slope + leftmost.first->getStrip();
 
+        //Extend from rightmost to layer
+        float rightslope = (rightmost.first->getStrip() - rightmost.second->getStrip())/(rightmost.first->getLayer() - rightmost.second->getLayer());
+        float rightedge = (layer - rightmost.first->getLayer())*slope + rightmost.first->getStrip();
+
+        //Arithmetic mean of right edge and left edge
+        float centertrack = (leftedge+rightedge)/2;
+        
+        //Define lowstrip and upstrip
+        int lowstrip = static_cast<int>(std::floor( centertrack - trackwidth_/2.0 ));
+        int upstrip = static_cast<int>(std::ceil( centertrack + trackwidth_/2.0 ));
+        CorrectStrip( lowstrip );
+        CorrectStrip( upstrip );
+
+        //SearchByKey
+        int lowkey = layer*layermod_ + lowstrip;
+        int upkey = layer*layermod_ + upstrip;
+        
+        return ( SearchByKey( lowkey , upkey , track ) );
     }
     
     bool HitLog::isAcceptableTrack( const std::vector< HitPtr > track ) const {
-
+        //For now, accepting all tracks
+        return true;
     }
 
     bool HitLog::SearchByKey( const int lowkey , const int upkey , std::vector< HitPtr > &track ) const {
