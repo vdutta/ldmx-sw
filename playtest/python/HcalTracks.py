742ac7e99de37a58b481446116c28677c1185e04
tomeichlersmith <eichl008@umn.edu>
2018-06-27 10:29:43 -0500

    first draft python config file for HcalTrackProducer

---
+++ playtest/python/HcalTracks.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+# we need the ldmx configuration package to construct the object
+from LDMX.Framework import ldmxcfg
+
+HcalTracks = ldmxcfg.Producer("HcalTracks", "ldmx::HcalTrackProducer")
+
+# name of collection that contains the hcal hits 
+HcalTracks.parameters["HitCollectionName"] = "hcalDigis"
+
+# hcal specifications
+HcalTracks.parameters["NumHcalLayers"] = 81 
+HcalTracks.parameters["NumHcalStrips"] = 34
+
+# minimum PE for a single hit to be considered non-noise
+HcalTracks.parameters["MinimumPE"] = 3.5
+
+# Search Cone around seed specifications
+#  Depth - number of layers to search away from seed
+#  Angle - number of strips to search across first layer away from seed
+#  Minimum Hits - minimum number of hits needed to be found around seed
+HcalTracks.parameters["SearchConeDepth"] = 3
+HcalTracks.parameters["SearchConeAngle"] = 3
+HcalTracks.parameters["MinConeHits"] = 3
+
+# Width of search in each layer (by number of strips) when extending track
+HcalTracks.parameters["TrackWidth"] = 3
+
