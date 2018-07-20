b41b73d292a268b6fc70bda560af6471f85e7345
tomeichlersmith <eichl008@umn.edu>
2018-07-20 10:49:25 -0500

    delete commented out histograms

--- Hcal/include/Hcal/HcalTrackAnalyzer.h
+++ Hcal/include/Hcal/HcalTrackAnalyzer.h
@@ -54,16 +54,6 @@ namespace ldmx {
             
             TH1F* h_tracksperevent_; //* number of tracks per event
             TH1F* h_layhitspertrack_[3]; //* number of layer hits per track, index i is the ith track found in the event
-/* Histograms to study track behavior
-            TH1F* h_pe_nonnoise_; //* pe distribution of all non-noise hits
-            TH1F* h_pe_notrack_; //* pe distribution of all hits not in a track
-
-            TH1F* h_energy_nonnoise_; //* energy distribution of all non-noise hits
-            TH1F* h_energy_notrack_; //* energy distribution of all hits not in a track
-
-            TH1F* h_strip_nonnoise_; //* strip number distribution of all non-noise hits
-            TH1F* h_strip_notrack_; //* strip number distribution of all hits not in a track
-*/
     };
 }
 
