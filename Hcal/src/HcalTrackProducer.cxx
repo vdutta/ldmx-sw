e3d883eb45637467a1ae0a144eb18d6113e05353
tomeichlersmith <eichl008@umn.edu>
2018-07-25 14:00:17 -0500

    wrote outline of new algorithm in comments

--- Hcal/src/HcalTrackProducer.cxx
+++ Hcal/src/HcalTrackProducer.cxx
@@ -53,6 +53,7 @@ namespace ldmx {
         //initialize event containters
         nonoiseLog_.clear();
         mipLog_.clear();
+        trackLog_.clear();
     
         //obtain list of hits
         const TClonesArray *rawhits = event.getCollection( hitcollname_ , hitpassname_ );
@@ -101,27 +102,12 @@ namespace ldmx {
             cgroup.push_back( itH->second );
         } //iterate through rawLog_, grouping hits into mip hits (itH)
       
-        //search for tracks
-        HcalTrack *track = new HcalTrack();
-        int trackcnt = 0;
-        while( TrackSearch( track ) and trackcnt < maxtrackcnt_ ) {
-            //add track to collection
-            HcalTrack *toadd = (HcalTrack *)(hcaltracks_->ConstructedAt(trackcnt));
-            *toadd = *track;
-            
-            //Remove track from log
-            RemoveTrack( track );
-
-            track->Clear(); //re-initialize track
-            seedlayer_ = *layercheck_.begin(); //change seedlayer
-            trackcnt++;
-        } //keep searching for tracks until can't find anymore
+        //search for tracks in each section
+        //merge tracks from each section into full detector tracks
+        //add full detector tracks to collection
         
         //add collection to event bus
         event.add( hcaltracksname_ , hcaltracks_ );
-        
-        //memory clean up
-        delete track;
         
         /* Skim Rule depending on number of tracks
         if ( trackcnt > 1 ) {
@@ -133,25 +119,14 @@ namespace ldmx {
         return;
     }
     
-    bool HcalTrackProducer::TrackSearch( HcalTrack *track ) {
+    bool HcalTrackProducer::TrackSearch( HcalSection section ) {
         
-        seedstrip_ = 0;
-        while ( FindSeed() ) { //seed found
-            
-            track->setSeed( seedlayer_ , seedstrip_ );
-            
-            SetSearchCone();
-            
-            //Checks if track is started successfully and then tries to
-            // extend the track.
-            if ( BeginPartialTrack( track ) and ExtendTrack( track ) ) { //track successfully constructed
-                return true;
-            }
-            
-            //bad seed
-            badseeds_.insert( KeyGen( 0 , seedlayer_ , seedstrip_ ) );
-
-        } //while possible good seed found
+        //Determine two end points
+        //Find all mips within traversing cylinder
+        //Check if track is acceptable
+        // YES: add track to trackLog and remove hits from mipLog
+        // NO: add end points to badendpoints set
+        //Repeat until out of possible end point pairs
 
         return false;
     }
@@ -173,146 +148,34 @@ namespace ldmx {
         int lowstrip = mip->getLowStrip();
         return KeyGen( section , layer , lowstrip );
     }
-    
-    bool HcalTrackProducer::FindSeed() {
-        
-        if ( layercheck_.empty() ) { //no more layers to check
-            return false;
-        }
-       
-        //check if seedlayer_ has been searched before
-        std::set<int>::iterator seedlayer_it = layercheck_.find( seedlayer_ );
 
-        if ( seedlayer_it != layercheck_.end() ) { //seedlayer_ hasn't been searched before
-           
-            //set keys to cover entire layer
-            int lowkey = seedlayer_*layermod_;
-            int upkey = (seedlayer_+1)*layermod_ - 1;
-            
-            HcalTrack *trackseed = new HcalTrack(); //temp HcalTrack for SearchByKey
-            int seedkey = lowkey - 1; //preserve lowkey when entering loop
-            bool badseedfound, mipfound;
-           
-            do { //SearchByKey found a mip and it is listed as a bad seed
-                lowkey = seedkey+1;//move range up to start after bad seed
-                mipfound = SearchByKey( lowkey , upkey , trackseed );//search for another mip 
-                if ( mipfound ) { //mip found
-                    seedstrip_ = static_cast<int>( trackseed->getHit(0)->getStrip() ); //reset seedstrip
-                    seedkey = KeyGen( 0 , seedlayer_ , seedstrip_ ); //reset seedkey
-                    badseedfound = ( badseeds_.find( seedkey ) != badseeds_.end() ); //see if new seed is bad
-                } else {
-                    badseedfound = false; //no mip found so exit loop
-                }
-                trackseed->Clear();
-            } while ( badseedfound ); //SearchByKey found a mip and it is listed as a bad seed
-            
-            if ( mipfound ) { //loop exited with a mipfound, then it is not a bad seed
-                return true;
-            }
-            
-            //entire layer searched, no good seeds
-            layercheck_.erase( seedlayer_it );
-
-        } //seedlayer_ hasn't been searched before
-
-        //Function would exit by now if it hadn't found a seed
-        seedlayer_ = *layercheck_.begin();
-        return ( FindSeed() );
-    }
-    bool HcalTrackProducer::BeginPartialTrack( HcalTrack *track ) {
-        
-        //make sure track is empty
-        if ( track->getNHits() > 0 ) {
-            track->Clear();
-        }
-
-        while ( !cone_.empty() ) { //loop through cone to find mips
-            
-            SearchByKey( cone_.front().first , cone_.front().second , track );
-            cone_.pop_front();
-
-        } //loop through cone to find mips
-
-        //check if enough hits in cone
-        return ( track->getNHits() >= minconehits_ );
-    }
-            
-    bool HcalTrackProducer::ExtendTrack( HcalTrack *track ) {
-        
-        //Iterate through list of layers to search for hits
-        bool addednewhit = true;
-        while ( !layerlist_.empty() ) { //loop through elements of layerlist_
-            
-            int layer = layerlist_.front();
-            layerlist_.pop_front();
-            
-            //Linearly extrapolate points to layer
-            // HcalTrack::evalFit calculates fit and then evaluates
-            float centerstrip = track->evalFit( layer );
-            
-            //Define lowstrip and upstrip
-            int lowstrip = static_cast<int>(std::floor( centerstrip - trackwidth_/2.0 ));
-            int upstrip = static_cast<int>(std::ceil( centerstrip + trackwidth_/2.0 ));
-            CorrectStrip( lowstrip );
-            CorrectStrip( upstrip );
-
-            //SearchByKey
-            int lowkey = KeyGen( 0 , layer , lowstrip );
-            int upkey = KeyGen( 0 , layer , upstrip );
-
-            addednewhit = SearchByKey( lowkey , upkey , track , centerstrip );
-            
-        } //loop through elements of layerlist
-
-        return isAcceptableTrack( track );
-    }
-    
-    bool HcalTrackProducer::isAcceptableTrack( const HcalTrack *track ) const {
-        //For now, only going by number of layer hits in track
-        return ( track->getNLayHits() > mintracklayhits_ );
-    }
-
-    bool HcalTrackProducer::SearchByKey( const std::map< int , MipHitPtr > log , const int lowkey , const int upkey , HcalTrack *track , const float prefstrip ) {
+    bool HcalTrackProducer::findEndPts( HcalSection section , std::pair< MipHitPtr > &endpts ) {
         
         bool success = false;
 
-        if ( lowkey > upkey ) { //Mis-use correction
-            std::cout << "Input hit keys to HcalTrackProducer::SearchByKey in wrong order: " << lowkey << "->" << upkey << std::endl;
-            std::cout << "Returning an empty search" << std::endl;
-        } else { //inputs are correct form
-            
-            auto lowbound = log.lower_bound( lowkey ); //points to first key that is not before lowkey (equivalent or after) (map::end if all are before lowkey)
-            auto upbound = log.upper_bound( upkey ); //points to first key after upkey (map::end if nothing after upkey)
+        if ( !mipLog_[section].empty() ) { //mip log for this section is nonempty
+            //Get first mip
+            //Iterate through log finding mip furthest from start that isn't in badEndPts
+            //Call this mip end
+            //Iterate through log finding mip furthest from end that isn't in badEndPts
+            //Call this mip start
+            //Return pair
+        } //miplog for this section is non emtpy
 
-            //count number of mips and determine which one is closest to prefstrip
-            int nmips = 0;
-            MipHitPtr bestmip;
-            float beststripdif = 10000.0;
-            for( auto it = lowbound; it != upbound; ++it ) {
-                nmips++;
-                MipHitPtr cmip = it->second;
-                float cstrip = static_cast<float>(cmip->getLowStrip() + cmip->getUpStrip())/2.0;
-
-                float cstripdif = std::abs( prefstrip - cstrip );
-
-                if ( cstripdif < beststripdif ) {
-                    beststripdif = cstripdif;
-                    bestmip = cmip;
-                } //is cmip closer than bestmip to prefstrip
-            } //iterate through [lowbound, upbound) range of log (it)
-        
-            //cases based on how many mips were found
-            if ( nmips > 0 ) { //there are some mips
-                
-                track->incLayHit();
-                track->addGroup( *bestmip );
-                success = true;
-
-            } //some mips were found
-
-        } //make sure inputs are correct
-        
         return success;
+    }
+    
+    bool HcalTrackProducer::connectTrack( HcalSection section , const std::pair< MipHitPtr > &endpts , std::vector< MipHitPtr > &possibletrack ) {
+        
+        //draw line between end points
+        //iterate through log collecting mips that are within trackwidth of line
+
+        return isAcceptableTrack( possibletrack );
+    }
+
+    bool HcalTrackProducer::isAcceptableTrack( const std::vector< MipHitPtr > &track ) const {
+        //For now, keeping all tracks
+        return ( true );
     }
 
     bool HcalTrackProducer::isMIP( const std::vector< HitPtr > &group ) const {
