b249d1ddba8b6f545fb17fa227412a93eae258e0
tomeichlersmith <eichl008@umn.edu>
2018-07-26 14:21:44 -0500

    major progress on trigger producer, although still uncompiled

--- Hcal/include/Hcal/HcalMipTriggerProducer.h
+++ Hcal/include/Hcal/HcalMipTriggerProducer.h
@@ -1,7 +1,7 @@
 /**
  * @file HcalMipTriggerProducer.h
- * @brief
- * @author
+ * @brief Header file for HcalMipTriggerProducer
+ * @author Tom Eichlersmith, University of Minnesota
  */
 
 #ifndef HCAL_HCALMIPTRIGGERPRODUCER_H
@@ -11,12 +11,13 @@
 #include "Event/Event.h"
 #include "Framework/EventProcessor.h" //Needed to declare processor
 #include "Framework/ParameterSet.h" // Needed to import parameters from configuration file
+#include "DetDescr/HcalID.h" //For HcalSection enum and HcalID creation
 
 namespace ldmx {
     
     /**
      * @class HcalMipTriggerProducer
-     * @brief 
+     * @brief Constructs Trigger Result depending on Section, Layer, Strip, and Amplitude information of hits in the Hcal.
      */
     class HcalMipTriggerProducer : public ldmx::Producer {
         public:
@@ -36,7 +37,73 @@ namespace ldmx {
             virtual void onProcessEnd();
 
         private:
+            
+            /**
+             * Determine if an HcalHit could be a MIP passing through.
+             * Checks on mininimum PE and maximum Energy.
+             *
+             * This function could be changed to check a two bit energy
+             * flag instead of checking against actual PE and Energy
+             * measurements.
+             *
+             * @param hit HcalHit* that needs to be checked
+             * @bool true if plausible MIP
+             */
+            bool isPlausibleMip( HcalHit* hit ) const;
 
+            /**
+             * Find end points that haven't been tested before.
+             * 
+             * @param orientation HcalOrientation of orientation to search
+             * @return true if new end points are found
+             */
+            bool findEndPoints( HcalOrientation orientation );
+
+            /**
+             * Remove a given raw ID value from the hit log.
+             *
+             * @param orientation HcalOrienation
+             * @param rawID raw ID value to be removed
+             */
+            void removeHcalID( HcalOrienation orientation , DetectorID::RawValue rawID );
+
+            /** enum for layers orientated in the same way */
+            enum HcalOrientation {
+                BACK_EVEN = 0,
+                BACK_ODD = 1,
+                TOP = 2,
+                BOTTOM = 3,
+                LEFT = 4,
+                RIGHT = 5
+            };
+            
+            /** list of orientation enum */
+            static const std::vector< HcalOrientation > HcalOrientationList_({ BACK_EVEN , BACK_ODD , TOP , BOTTOM , LEFT , RIGHT })
+            
+            /** Hits sorted by their orientations and stored as their raw IDs */
+            std::map< HcalOrientation , std::map< DetectorID::RawValue , HcalID > > hitLog_;
+
+            /** Bad End Points for each HcalOrientation */
+            std::map< HcalOrientation , std::set< DetectorID::RawValue > > badEndPts_;
+
+            /** Current Starting Point */
+            HcalID* startPt_;
+
+            /** Current Finish Point */
+            HcalID* finishPt_;
+            
+            /** Name of HcalHit collection and pass */
+            std::string hitCollName_;
+            std::string hitPassName_;
+
+            /** Minimum Number of Layers Hit to be considered a MIP */
+            int minLayersHit_;
+
+            /** Maximum Energy of a HcalHit to be considered a MIP */
+            float maxEnergy_;
+
+            /** Minimum PE of a HcalHit to be considered a real hit (not noise) */
+            float minPE_;
     };
 }
 
--- Hcal/src/HcalMipTriggerProducer.cxx
+++ Hcal/src/HcalMipTriggerProducer.cxx
@@ -14,6 +14,94 @@ namespace ldmx {
     }
 
     void HcalMipTriggerProducer::produce(const ldmx::Event& event) {
+        
+        //initialize event containers
+        hitLog_.clear();
+        badEndPts_.clear();
+        
+        //obtain list of raw hits
+        const TClonesArray *rawhits = event.getCollection( hitCollName_ , hitPassName_ );
+
+        //add only plausible mip hits to hitLog_
+        for ( size_t iH = 0; iH < rawhits->GetEntriesFast(); iH++ ) {
+            HcalHit* chit = (HcalHit*)(rawhits->At(iH));
+            if ( isPlausibleMip( chit ) ) { //could be a MIP
+                
+                HcalSection csect = chit->getSection();
+                int clayer = chit->getLayer();
+                HcalOrientation corient = csect;
+                
+                //The underlying ints for the HcalSection and HcalOrientation enums are shifted by one
+                // except for EVEN layers in the BACK Hcal.
+                if ( csect != HcalSection::BACK or (csect = HcalSection::BACK and clayer % 2 == 1 ) ) {
+                    corient++;
+                }
+                
+                //Each hit in hitLog_[ orientation ] has the same section number, so the standard sort
+                // will sort them correctly according to layer,strip information
+                HcalID cID;
+                cID.setRawValue( chit->getID() );
+                cID.unpack();
+                hitLog_[ corient ][ chit->getID() ] = cID;
+            } //could be a MIP
+        } //iterate through rawhits (iH)
+
+        //for each HcalOrientation
+        for ( HcalOrientation corient : HcalOrientationList_ ) {
+            
+            //while good end points are being found
+            int trackcnt = 0;
+            while ( findEndPoints( corient ) ) {
+                
+                int laycnt = 0;
+                if ( startPt_->getLayerID() != finishPt_->getLayerID() ) {
+                    
+                    //calculate slope
+                    int startLayer = startPt_->getLayerID();
+                    int startStrip = startPt_->getStrip();
+                    int dstrip = ( finishPt_->getStrip() - startStrip );
+                    int dlayer = ( finishPt_->getLayerID() - startLayer );
+                    float slope = static_cast<float>(dstrip)/static_cast<float>(dlayer);
+                    
+                    //count hits in this orientation that are in the cylinder
+                    std::set<int> countedLayers; //layers that already have been counted
+                    for ( auto it : hitLog_[ corient ] ) {
+                        
+                        HcalID* chit = &( it->second );
+    
+                        int cstrip = chit->getStrip();
+                        int clayer = chit->getLayerID();
+    
+                        float trackstrip = slope*( clayer - startLayer ) + startStrip;
+    
+                        float stripdif = std::abs( trackstrip - cstrip );
+    
+                        if ( stripdif < maxStripDif_ ) {
+                            //hit is in track cylinder
+                            if ( countedLayers.find( clayer ) != countedLayers.end() )
+                                laycnt++;
+    
+                            track.push_back( chit->getRawValue() );
+                        } //check if hit is in track cylinder
+    
+                    } //iterate through hitLog of current orientation (chit)
+                
+                } //startPt and finishPt are not on the same layer
+
+                if ( laycnt > minLayersHit_ ) {
+                    //good track found
+                    for ( DectorID::RawValue hcalID : track ) {
+                        removeHcalID( corient , hcalID );
+                    } //iterate through track (hcalID)
+                    
+                    trackcnt++;
+
+                } else {
+                    badEndPts_.insert( startPt_->getRawValue() );
+                    badEndPts_.insert( finishPt_->getRawValue() );
+                } //what to do if track is good
+
+            } //while good end points are still being found
 
         return;
     }
@@ -34,6 +122,59 @@ namespace ldmx {
     }
 
     void HcalMipTriggerProducer::onProcessEnd() {
+
+        return;
+    }
+
+    bool HcalMipTriggerProducer::isPlausibleMip( HcalHit* hit ) const {
+        return ( hit->getPE() > minPE_ and hit->getEnergy() < maxEnergy_ );
+    }
+
+    bool HcalMipTriggerProducer::findEndPoints( HcalOrientation orientation ) {
+        
+        startPt_ = nullptr;
+        finishPt_ = nullptr;
+
+        if ( hitLog_[orientation].size() > 1 ) {
+            
+            for ( auto itH : hitLog_[ orientation ] ) {
+
+                HcalID *chit = &( itH->second );
+                int clayer = chit->getLayerID();
+
+                bool isGoodEndPt = ( badEndPts.find( itH->first ) == badEndPts.end() );
+
+                if ( startPt_ ) {
+                    
+                    if ( clayer < startPt_->getLayerID() and isGoodEndPt ) {
+                        startPt_ = chit;
+                    } //if chit could be a good start point
+
+                } else {
+                    startPt_ = chit;
+                } //if startPt_ is nullptr
+
+                if ( finishPt_ ) {
+                    
+                    if ( clayer > finishPt_->getLayerID() and isGoodEndPt ) {
+                        finishPt_ = chit;
+                    } //if chit could be a good finish point
+
+                } else {
+                    finishPt_ = chit;
+                } //if finishPt_ is nullptr
+            
+            } //iterate through hits in this orientation hitlog (chit)
+        
+        } //two ore more hits in this part of the log
+
+        return ( startPt_ and finishPt_ );
+
+    }
+
+    void HcalMipTriggerProducer::removeHcalID( HcalOrientation orientation , DetectorID::RawValue rawID ) {
+        
+        hitLog_[ orientation ].erase( rawID );
 
         return;
     }
