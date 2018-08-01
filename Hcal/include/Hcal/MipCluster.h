ba4c50e251d71e7200017bd06435253774b0f266
tomeichlersmith <eichl008@umn.edu>
2018-08-01 12:52:30 -0500

    MipCluster clean up, delete old code, new start

--- Hcal/include/Hcal/MipCluster.h
+++ Hcal/include/Hcal/MipCluster.h
@@ -1,144 +1,67 @@
 /**
- * @file MipHit.h
+ * @file MipCluster.h
  * @author Tom Eichlersmith, University of Minnesota
- * @brief Header file for class MipHit
+ * @brief Header file for class MipCluster
  */
 
-#ifndef HCAL_MIPHIT_H
-#define HCAL_MIPHIT_H
+#ifndef HCAL_MIPCLUSTER_H
+#define HCAL_MIPCLUSTER_H
 
 //Standard Libraries
-#include <vector> //for storage fector of HcalHit pointers
+#include <vector> //for storage vector of HcalHit pointers
 
 //ROOT Libs
 
 //LDMX Framework
-#include "Event/HcalTrack.h" //for typedef of hcalhits
 #include "DetDescr/HcalID.h" //for the HcalSection enum
 
 namespace ldmx {
     
     /**
-     * @class MipHit
+     * @class MipCluster
      * @brief Stores pointers to HcalHits that are considered a single MIP hit (usually due to proximity).
      */
-    class MipHit {
+    class MipCluster {
         public:
             /**
              * Default Constructor
              */
-            MipHit();
+            MipCluster();
 
             /**
-             * Preferred Constructor
-             * Uses input hit list to initialize mip.
+             * Add an HcalHit to the MipCluster
              */
-            MipHit( std::vector< HitPtr > hcalHits );
-
-            /**
-             * Add an HcalHit to the MipHit
-             */
-            void addHit( HitPtr hit );
+            void addHit( ldmx::HcalHit* hit );
             
             /**
-             * Set Up this MipHit. Once this MipHit has the HcalHits added to it,
-             *  running this function will calculate the other member variables from the
-             *  HcalHits stored in this class.
-             * 
-             * @return true if successful
-             */
-            bool setUp();
-
-            /**
-             * Get the section.
-             */
-            int getSection() const { return section_; }
-
-            /**
-             * Get the layer.
-             */
-            int getLayer() const { return layer_; }
-
-            /**
-             * Get the lower strip.
-             */
-            int getLowStrip() const { return lowstrip_; }
-
-            /**
-             * Get the upper strip.
-             */
-            int getUpStrip() const { return upstrip_; }
-            
-            /**
-             * Get the total energy of the MipHit
+             * Get the total energy of the MipCluster
              */
             float getEnergy() const { return totalEnergy_; }
 
             /**
-             * Get the number of HcalHits in this MipHit
+             * Get the number of HcalHits in this MipCluster
              */
             int getNumHits() const { return hcalHits_.size(); }
 
             /**
-             * Get MipHit box center
+             * Get vector of HcalHits in this MipCluster
              */
-            const std::vector<float> &getBoxCenter() const { return boxCenter_; }
-
-            /**
-             * Get vector of HcalHits in this MipHit
-             */
-            const std::vector< HitPtr > &getHcalHits() const { return hcalHits_; }
+            const std::vector< ldmx::HcalHit* > &getHcalHits() const { return hcalHits_; }
 
         private:
             
             /**
-             * Determine the Section, Layer, Strip information and check if it is consistent.
-             * All hits are in the same section and strips don't differ by more than one.
-             */
-            bool setSLS();
-
-            /**
-             * Determine the six planes of the MipHit box in detector coordinates.
-             *
-             * @return true if successful
-             */
-            bool setBoxPlanes();
-
-            /**
-             * Calculate the center of the MipHit box in detector coordinates.
-             */
-            void setBoxCenter();
-
-            /**
-             * Calculate the total energy of the MipHit.
+             * Calculate the total energy of the MipCluster.
              */
             void setTotalEnergy();
-
-            /** Section of the Hits */
-            int section_;
-
-            /** Layer of the Hits */
-            int layer_;
-
-            /** low and up strip of Hits (may be equal) */
-            int lowstrip_, upstrip_;
-
-            /** The total energy of the MipHit */
+            
+            /** The total energy of the MipCluster */
             float totalEnergy_;
-
-            /** The center of the MipHit box in detector coordinates */
-            std::vector< float > boxCenter_;
-
-            /** The planes of the MipHit box in detector coordinates */
-            float xMin_, xMax_ , yMin_, yMax_, zMin_, zMax_;
  
             /** Storage vector of pointers to HcalHits */
-            std::vector< HitPtr > hcalHits_;
+            std::vector< ldmx::HcalHit* > hcalHits_;
    };
-
-    /** Pointer to MipHit instance */
-    typedef MipHit * MipHitPtr;
 
 }
 
-#endif /* HCAL_MIPHIT_H */
+#endif /* HCAL_MIPCLUSTER_H */
--- Hcal/src/MipCluster.cxx
+++ Hcal/src/MipCluster.cxx
@@ -1,81 +1,21 @@
 /**
- * @file MipHit.cxx
- * @brief Implementation file for class MipHit
+ * @file MipCluster.cxx
+ * @brief Implementation file for class MipCluster
  * @author Tom Eichlersmith, University of Minnesota
  */
 
-#include "Hcal/MipHit.h"
+#include "Hcal/MipCluster.h"
 
 namespace ldmx {
 
-    MipHit::MipHit() : section_(0), layer_(0), lowstrip_(0), upstrip_(0), totalEnergy_(0.0),
-        boxCenter_( 3 , -1.0 ), xMin_(0.0), xMax_(0.0), yMin_(0.0), yMax_(0.0), zMin_(0.0), zMax_(0.0) { }
+    MipCluster::MipHit() : totalEnergy_(0.0) { }
 
-    MipHit::MipHit( std::vector< HitPtr > hcalHits ) : MipHit() {
-        for ( HcalHit* hit : hcalHits ) {
-            addHit( hit );
-        }
-    }
-
-    void MipHit::addHit( HitPtr hit ) {
+    void MipCluster::addHit( ldmx::HcalHit* hit ) {
         hcalHits_.push_back( hit );
         return;
     }
 
-    bool MipHit::setUp() {
-        
-        setTotalEnergy();
-
-        if ( !setBoxPlanes() ) {
-            return false;
-        }
-
-        setBoxCenter();
-        
-        return true;
-    }
-
-    bool MipHit::setSLS() {
-        
-        layer_ = static_cast<int>(hcalHits_[0]->getLayer());
-        section_ = hcalHits_[0]->getSection();
-        lowstrip_ = hcalHits_[0]->getStrip();
-        upstrip_ = hcalHits_[0]->getStrip();
-        for ( auto it = hcalHits_.begin(); it != hcalHits_.end(); ++it ) {
-            if ( static_cast<int>((*it)->getLayer()) != layer_ ) {
-                std::cout << "[ MipHit::setBoxCorners ] : Different layers were grouped into the same MipHit." << std::endl;
-                return false;
-            } else if ( (*it)->getSection() != section_ ) {
-                std::cout << "[ MipHit::setBoxCorners ] : Different sections were grouped into the same MipHit." << std::endl;
-                return false;
-            }
-            
-            int cstrip = (*it)->getStrip();
-            if ( cstrip < lowstrip_ ) {
-                lowstrip_ = cstrip;
-            } else if ( cstrip > upstrip_ ) {
-                upstrip_ = cstrip;
-            } //calculate low,up strip
-        } //check that all hits are in the same section,layer (it) and set low,up strip
-        
-        return true;
-    }
-    
-    bool MipHit::setBoxPlanes() {
-        
-        bool success = false;
-        
-        return success;
-    }
-
-    void MipHit::setBoxCenter() {
-        boxCenter_[0] = (xMin_ + xMax_)/2;
-        boxCenter_[1] = (yMin_ + yMax_)/2;
-        boxCenter_[2] = (zMin_ + zMax_)/2;
-        return;
-    }
-
-    void MipHit::setTotalEnergy() {
+    void MipCluster::setTotalEnergy() {
         totalEnergy_ = 0.0;
         for ( unsigned int i = 0; i < hcalHits_.size(); i++ ) {
             totalEnergy_ += hcalHits_[i]->getEnergy();
