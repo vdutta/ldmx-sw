3e43751fe5c322be73c248cd61fc10e325ae17f4
tomeichlersmith <eichl008@umn.edu>
2018-08-02 11:50:55 -0500

    new reconstruction producer

---
+++ Hcal/include/Hcal/HcalMipTrackProducer.h
@@ -0,0 +1,43 @@
+/**
+ * @file HcalMipTrackProducer.h
+ * @brief Header file for HcalMipTrackProducer class
+ * @author Tom Eichlersmith, University of Minnesota
+ */
+
+#ifndef HCAL_HCALMIPTRACKPRODUCER_H
+#define HCAL_HCALMIPTRACKPRODUCER_H
+
+//LDMX Framework
+#include "Event/Event.h"
+#include "Framework/EventProcessor.h" //Needed to declare processor
+#include "Framework/ParameterSet.h" // Needed to import parameters from configuration file
+
+namespace ldmx {
+    
+    /**
+     * @class HcalMipTrackProducer
+     * @brief Producer that reconstructs MIP tracks through the hcal.
+     */
+    class HcalMipTrackProducer : public ldmx::Producer {
+        public:
+
+            HcalMipTrackProducer(const std::string& name, ldmx::Process& process) : ldmx::Producer(name, process) {}
+
+            virtual void configure(const ldmx::ParameterSet& ps);
+
+            virtual void produce(ldmx::Event& event);
+
+            virtual void onFileOpen();
+
+            virtual void onFileClose();
+
+            virtual void onProcessStart(); 
+
+            virtual void onProcessEnd();
+
+        private:
+
+    };
+}
+
+#endif /* HCAL_HCALMIPTRACKPRODUCER_H */
---
+++ Hcal/src/HcalMipTrackProducer.cxx
@@ -0,0 +1,43 @@
+/**
+ * @file HcalMipTrackProducer.cxx
+ * @brief Implementation file for HcalMipTrackProducer class.
+ * @author Tom Eichlersmith, University of Minnesota
+ */
+
+#include "Hcal/HcalMipTrackProducer.h"
+
+namespace ldmx {
+
+    void HcalMipTrackProducer::configure(const ldmx::ParameterSet& ps) {
+
+        return;
+    }
+
+    void HcalMipTrackProducer::produce(ldmx::Event& event) {
+
+        return;
+    }
+    
+    void HcalMipTrackProducer::onFileOpen() {
+
+        return;
+    }
+
+    void HcalMipTrackProducer::onFileClose() {
+
+        return;
+    }
+
+    void HcalMipTrackProducer::onProcessStart() {
+
+        return;
+    }
+
+    void HcalMipTrackProducer::onProcessEnd() {
+
+        return;
+    }
+
+}
+
+DECLARE_PRODUCER_NS(ldmx, HcalMipTrackProducer);
