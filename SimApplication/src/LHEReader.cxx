4817f9a5dc231344b3483e8ff218d186ec272e30
omar-moreno <email@omarmoreno.net>
2018-08-02 17:11:49 -0700

    Update reader so it can read both GiBUU and GENIE files.

--- SimApplication/src/LHEReader.cxx
+++ SimApplication/src/LHEReader.cxx
@@ -35,20 +35,24 @@ namespace ldmx {
 
         LHEEvent* nextEvent = new LHEEvent(line);
 
+        // Process all of the particles listed within the event block along 
+        // with any specified auxiliary information. 
         while (getline(ifs_, line)) {
 
-            if (line == "</event>") {
-                break;
-            }
+            // Break out of the while loop when the end of the event block
+            // is reached.
+            if (line == "</event>") break;
 
-	    if (line.find("#")==std::string::npos) {	    // not a comment line
-		LHEParticle* particle = new LHEParticle(line);
-		nextEvent->addParticle(particle);
-	    } else {
-		if (line.find("#vertex")!=std::string::npos) {
-		    nextEvent->setVertex(line);
-		}
-	    }
+		    // The creators of the LHE format recommends preceding auxiliary 
+            // information by a # symbol.  For LDMX, the vertex position of a 
+            // particle is added this way.
+            if (line.find_first_of("#") != std::string::npos) { 
+                nextEvent->setVertex(line);
+            // All other lines of the LHE file are translated into particles
+            } else { 
+                LHEParticle* particle = new LHEParticle(line);
+		        nextEvent->addParticle(particle);
+            }
         }
 
         const std::vector<LHEParticle*>& particles = nextEvent->getParticles();
