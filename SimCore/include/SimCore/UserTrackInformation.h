cb73e3b9ac18d9e669ceb482cad13f22c7628224
omar-moreno <email@omarmoreno.net>
2019-03-15 15:49:49 -0700

    Add a flag that allows tagging of a track as a brem candidate.

--- SimCore/include/SimCore/UserTrackInformation.h
+++ SimCore/include/SimCore/UserTrackInformation.h
@@ -1,11 +1,16 @@
 /**
  * @file UserTrackInformation.h
  * @brief Class providing extra information associated to a Geant4 track
- * @author Jeremy McCormick, SLAC National Accelerator Laboratory
+ * @author Jeremy McCormick
+ * @author Omar Moreno, SLAC National Accelerator Laboratory
  */
-#ifndef SIMCORE_USERTRACKINFORMATION_H_
-#define SIMCORE_USERTRACKINFORMATION_H_
 
+#ifndef _SIMCORE_USER_TRACK_INFORMATION_H_
+#define _SIMCORE_USER_TRACK_INFORMATION_H_
+
+//------------//
+//   Geant4   //
+//------------//
 #include "G4VUserTrackInformation.hh"
 #include "G4ThreeVector.hh"
 
@@ -19,55 +24,63 @@ namespace ldmx {
 
         public:
 
-            UserTrackInformation() {
-            }
-
-            virtual ~UserTrackInformation() {
-            }
-
-            /**
-             * Overload pure virtual method (we don't implement it!).
-             */
-            virtual void Print() const {;}
+            /** Overload pure virtual method (we don't implement it!). */
+            void Print() const {;}
 
             /**
              * Get the flag which indicates whether this track should be saved
              * as a Trajectory.
              * @return The save flag.
              */
-            bool getSaveFlag() { return saveFlag_; }
+            inline bool getSaveFlag() const { return saveFlag_; }
 
             /**
              * Set the save flag so the associated track will be persisted
              * as a Trajectory.
              * @param saveFlag True to save the associated track.
              */
-            void setSaveFlag(bool saveFlag) { saveFlag_ = saveFlag; }
+            inline void setSaveFlag(const bool& saveFlag) { saveFlag_ = saveFlag; }
 
             /**
              * Get the initial momentum 3-vector of the track [MeV].
              * @return The initial momentum of the track.
              */
-            const G4ThreeVector& getInitialMomentum() { 
-                return initialMomentum_; 
-            } 
+            inline const G4ThreeVector& getInitialMomentum() { return initialMomentum_; } 
            
             /**
              * Set the initial momentum of the associated track.
              * @param p The initial momentum of the track.
              */ 
-            void setInitialMomentum(const G4ThreeVector& p) { 
-                initialMomentum_.set(p.x(), p.y(), p.z()); 
-            } 
+            inline void setInitialMomentum(const G4ThreeVector& p) { initialMomentum_.set(p.x(), p.y(), p.z()); } 
         
+            /** 
+             * Set the flag used by TargetBremFilter to tag photon tracks 
+             * produced in the target via the eBrem process by electron 
+             * satisfying an energy threshold requirement.
+             * @param isBremCandidate True if the track is a brem whose parent
+             *                        is an electron below threshold.
+             */
+            inline void tagBremCandidate(const bool& isBremCandidate = true) { isBremCandidate_ = isBremCandidate; }
+
+            /**
+             * @return true if the track is a brem candidate. 
+             */
+            inline bool isBremCandidate() const { return isBremCandidate_; } 
+
+
         private:
 
             /** Flag for saving the track as a Trajectory. */
             bool saveFlag_{false};
 
+            /** Flag used to tag a track as a brem candidate. */
+            bool isBremCandidate_{false}; 
+
             /** The initial momentum of the track. */
             G4ThreeVector initialMomentum_;
-    };
-}
 
-#endif
+    };  // UserTrackInformation
+
+} // ldmx
+
+#endif // _SIMCORE_USER_TRACK_INFORMATION_H_
