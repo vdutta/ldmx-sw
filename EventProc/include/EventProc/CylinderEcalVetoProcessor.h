1b98ca14c32241ae26b92dd3aead0a752cac4466
omar-moreno <email@omarmoreno.net>
2019-05-01 15:47:30 -0700

    Class that computes 'cylinder/Nelson' features and determines if an event can be vetoed based on the results of a BDT classifier.

---
+++ EventProc/include/EventProc/CylinderEcalVetoProcessor.h
@@ -0,0 +1,149 @@
+/**
+ * @file CylinderEcalVetoProcessor.h
+ * @brief Class that computes feature vars for multi electron PN discrimination
+ * @author Andrew Whitbeck, FNAL
+ */
+
+#ifndef EVENTPROC_MULTIELECTRONVETO_H_
+#define EVENTPROC_MULTIELECTRONVETO_H_
+
+//----------------//
+//   C++ StdLib   //
+//----------------//
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+
+//----------//
+//   LDMX   //
+//----------//
+#include "DetDescr/EcalHexReadout.h"
+#include "DetDescr/EcalDetectorID.h"
+#include "Event/EcalHit.h"
+#include "Event/EventConstants.h"
+//#include "Event/MultiEleVetoResult.h"
+#include "Event/SimCalorimeterHit.h"
+#include "Event/SimTrackerHit.h"
+#include "Framework/EventProcessor.h"
+
+//----------//
+//   ROOT   //
+//----------//
+#include "TClonesArray.h"
+
+namespace ldmx {
+
+    class CylinderEcalVetoProcessor: public Producer {
+
+        public:
+
+            /** Constructor */
+            CylinderEcalVetoProcessor(const std::string& name, Process& process);
+
+            /** Destructor */
+            ~CylinderEcalVetoProcessor(); 
+
+            /** Read in user-specified parameters. */
+            void configure(const ParameterSet&);
+    
+            /** Run the multi-electron veto. */
+            void produce(Event& event);
+
+            /** 
+             * Get the HCal scoring plane hits associated with all recoil
+             * electrons.
+             *
+             * @param simParticles Collection of SimParticles in the event
+             * @param hcalSPHits Collection of HCal scoring plane hits
+             *
+             * @return Collection of HCal scoring plane hits associted with 
+             *         all recoil electrons present in the event.
+             *
+             */
+            std::vector<SimTrackerHit*> getRecoilElectronHcalSPHits(const TClonesArray* simParticles, const TClonesArray* HCalSPHits); 
+           
+            /** 
+             * Get all recoil electron in the event.
+             *
+             * @param Collection of SimParticles in the event
+             *
+             * @return Collection of recoil electrons in the event
+             */ 
+            std::vector<SimParticle*> getRecoilElectrons(const TClonesArray* simParticles);
+            
+            typedef std::pair<float, float> XYCoords;
+
+        private:
+
+            void Debug(std::string output){ 
+                if( verbose_ )
+                    std::cout << " [ CylinderEcalVetoProcessor ] : " << output << std::endl;
+            };
+
+            template <class T> void Debug(std::string output_label,std::vector<T> output_value){ 
+                if( verbose_ ){
+                    std::cout << " [ CylinderEcalVetoProcessor ] : " << output_label ;
+                    for( int i = 0 ; i < output_value.size() ; i++ ){
+                        std::cout << output_value[i] << ", " ;
+                    }
+                    std::cout << std::endl;
+                }
+            };
+
+            /** */
+            EcalHexReadout* hexReadout_{new EcalHexReadout{}};
+
+            /** Object used to persist veto variables and results. */
+            //MultiEleVetoResult result_;
+
+            /** Flag denoting whether verbose output is enabled/disabled. */
+            bool verbose_{false};
+
+            /** The Moliere radius. */
+            const double moliereR_{25./* mm */};
+
+            /** 
+             * The z position of each of the ECal layers. 
+             * TODO: These positions should eventually come from the geometry.
+             */
+            std::vector<double> layer_z{
+                223.8, // Layer 1
+                226.7, // Layer 2
+                233.05, // Layer 3
+                237.45, // Layer 4
+                245.3, // Layer 5
+                251.2, // Layer 6
+                260.3, // Layer 7
+                266.7, // Layer 8
+                275.8, // Layer 9
+                282.2, // Layer 10
+                291.3, // Layer 11
+                297.7, // Layer 12
+                306.8, // Layer 13
+                313.2, // Layer 14
+                322.3, // Layer 15
+                337.8, // Layer 16
+                344.2, // Layer 17
+                353.3, // Layer 18
+                359.7, // Layer 19
+                368.8, // Layer 20
+                375.2, // Layer 21
+                384.3, // Layer 22
+                390.7, // Layer 23
+                403.3, // Layer 24
+                413.2, // Layer 25
+                425.8, // Layer 26
+                435.7, // Layer 27
+                448.3, // Layer 28
+                458.2, // Layer 29
+                470.8, // Layer 30
+                480.7, // Layer 31
+                493.3, // Layer 32
+                503.2 // Layer 33
+            };
+    };
+}
+
+#endif
---
+++ EventProc/src/CylinderEcalVetoProcessor.cxx
@@ -0,0 +1,264 @@
+/**
+ * @file CylinderEcalVetoProcessor.cxx
+ * @brief Class that computes 'cylinder' features and determines if an event 
+ *        can be vetoed based on the results of a BDT classifier.
+ * @author Omar Moreno, SLAC National Accelerator Laboratory
+ * @author Andrew Whitbeck, Texas Tech
+ */
+
+#include "EventProc/CylinderEcalVetoProcessor.h"
+
+namespace ldmx {
+
+    CylinderEcalVetoProcessor::CylinderEcalVetoProcessor(const std::string &name, Process &process):
+        Producer(name, process) { 
+    }
+
+    CylinderEcalVetoProcessor::~CylinderEcalVetoProcessor() {
+        delete hexReadout_; 
+    }
+
+    void CylinderEcalVetoProcessor::configure(const ParameterSet& ps) {
+        //verbose_ = ps.getBool("verbose",false);
+    }
+
+    std::vector<SimParticle*> CylinderEcalVetoProcessor::getRecoilElectrons(const TClonesArray* simParticles) { 
+        
+        // Loop through all the sim particles and search for the recoil electrons 
+        // i.e. electrons which don't have any parents
+        std::vector<SimParticle*> recoils; 
+        for (int particleCount = 0; particleCount < simParticles->GetEntriesFast(); 
+                ++particleCount) { 
+            
+            // Get the ith particle from the collection of sim particles
+            SimParticle* particle = static_cast<SimParticle*>(simParticles->At(particleCount)); 
+
+            // If the particle is an electron and has no parents, a recoil has 
+            // been found
+            if ((particle->getPdgID() == 11) && (particle->getParentCount() == 0)) { 
+                recoils.push_back(particle);
+            }
+        }
+
+        // All events should contain at least 1 recoil electron
+        if (recoils.size() == 0) { 
+            throw std::runtime_error("Event doesn't contain any recoil electrons!");  
+        }
+
+        return recoils; 
+    }
+
+    std::vector<SimTrackerHit*> CylinderEcalVetoProcessor::getRecoilElectronHcalSPHits(
+            const TClonesArray* simParticles, const TClonesArray* spHits) { 
+    
+        // Collection of recoil electron HCal hits
+        std::vector<SimTrackerHit*> hits;
+
+        // Start by getting all recoil electrons in the event
+        std::vector<SimParticle*> recoils = this->getRecoilElectrons(simParticles); 
+
+        // Loop over the recoil electrons and find the associated HCal scoring 
+        // plane hit.
+        for (const auto& recoil : recoils) { 
+            
+            // Loop through all the HCal scoring plane hits and find the 
+            // one associated with the recoil electron.  Note: There may be 
+            // multiple hits associated with a recoil electron.  In this case,
+            // take the one that has the highest momentum and a positive pz 
+            // component.  Also, only consider hits produced in the front 
+            // HCal scoring plane.
+            double pMax = 0; 
+            SimTrackerHit* recoilHCalSPHit = nullptr; 
+            for( int iHit = 0 ; iHit < spHits->GetEntriesFast() ; iHit++ ) {
+                
+                // Get the ith hit in the collection
+                SimTrackerHit* hit = (SimTrackerHit*) spHits->At(iHit);
+               
+                    // Only consider hits associated with recoil electrons 
+                    if (hit->getSimParticle() == recoil) { 
+                        
+                        // Make sure the hit wasn't created by a backwards going 
+                        // recoil. 
+                        if (hit->getMomentum()[2] <= 0) continue;
+                        
+                        // Select the hit that has the highest momentum
+                        std::vector<double> pVec = hit->getMomentum();
+                        double p = sqrt(pVec[0]*pVec[0] + pVec[1]*pVec[1] + pVec[2]*pVec[2]); 
+                        if (pMax < p) { 
+                            pMax = p; 
+                            recoilHCalSPHit = hit;     
+                        }     
+                    }
+            }
+
+            hits.push_back(recoilHCalSPHit); 
+        }
+    
+    return hits;  
+    //std::vector<SimTrackerHit*> CylinderEcalVetoProcessor::getRecoilElectrons(Event& event){
+    //    const TClonesArray* simParticles = event.getCollection("SimParticles");
+    //    const TClonesArray* spHits = event.getCollection("HcalScoringPlaneHits");
+        
+        /*
+        std::vector<SimTrackerHit*> recoil_electrons;
+        std::sort(recoil_electrons.begin(),recoil_electrons.end(),compareSimTrackerHits);
+        for( int iPar = 0 ; iPar < simParticles->GetEntriesFast() ; iPar++ ){
+            SimParticle* simPar = (SimParticle*) simParticles->At(iPar);
+            //simPar->Print();
+            SimTrackerHit* recoil_electron=NULL;
+            if( simPar->getGenStatus()==1 and simPar->getPdgID()==11 ){
+                float max_hit_mom = 0.;
+                for( int iHit = 0 ; iHit < spHits->GetEntriesFast() ; iHit++ ){
+                    SimTrackerHit* hit = (SimTrackerHit*) spHits->At(iHit);
+                    std::vector<double> hit_mom_3vec = hit->getMomentum();
+                    double hit_mom = sqrt(pow(hit_mom_3vec[0],2)+
+                            pow(hit_mom_3vec[1],2)+
+                            pow(hit_mom_3vec[2],2));
+                    if( hit->getSimParticle() == simPar && max_hit_mom < hit_mom ){
+                        max_hit_mom = hit_mom;
+                        recoil_electron = hit;
+                    }
+                }// end loop over scoring plane hits
+
+            }// end if block for beam electrons
+            recoil_electrons.push_back(recoil_electron);
+        }// end loop over sim particles
+
+        return recoil_electrons;*/
+    }
+
+    void CylinderEcalVetoProcessor::produce(Event& event) {
+        
+        // Clear the previous result.
+        //result_.Clear(); 
+
+        // Get the collection of SimParticles from the event
+        const TClonesArray* simParticles = event.getCollection("SimParticles");
+
+        // Check for the existence of the collection of HCal scoring plane hits.
+        // If it does exist, use it to get the scoring plane position of the 
+        // recoils at the ECal/HCal face.  Otherwise, just use the ECal scoring
+        // plane hits.
+        const TClonesArray* spHits = nullptr; 
+        if (event.exists("HcalScoringPlaneHits")) { 
+            // Get the collection of HCal scoring planes hits from the event.
+            spHits = event.getCollection("HcalScoringPlaneHits");
+        } else { 
+            spHits = event.getCollection("EcalScoringPlaneHits");  
+        }
+
+        // Get the collection of digitized ECal hits from the event.
+        const TClonesArray* ecalDigis = event.getCollection("ecalDigis");
+
+        // Get the HCal scoring plane hits associated with all recoil electrons 
+        // in the event.
+        std::vector<SimTrackerHit*> recoils = this->getRecoilElectronHcalSPHits(simParticles, spHits);
+
+        // Loop over all of the recoil SP hits and calcualate the veto variables.
+        for(const auto& electron : recoils) {
+            
+            if (!electron) continue;
+
+            //result_.addElectron();
+
+            std::vector<double> pvec = electron->getMomentum();
+            Debug("electron momentum: ",pvec);
+            
+            std::vector<float> position = electron->getPosition();    
+            Debug("electron position: ",position);
+
+            // Get the collection of digitized Ecal hits from the event. 
+            int nEcalHits = ecalDigis->GetEntriesFast();
+
+            std::vector<double> cylinder_0_1(34,0.);
+            std::vector<double> cylinder_1_3(34,0.);
+            std::vector<double> cylinder_3_5(34,0.);
+            std::vector<double> cylinder_5(34,0.);
+
+            // loop over hits and collect them into cylinder sums
+            for (int iHit = 0; iHit < nEcalHits; iHit++) {
+
+                EcalHit* hit = (EcalHit*) ecalDigis->At(iHit);
+                
+                //hit->Print();
+                //int layer = hit->getLayer();
+                int rawID = hit->getID();
+                EcalDetectorID detId;
+                detId.setRawValue(rawID);
+                detId.unpack();
+                int layer = detId.getFieldValue("layer");
+                int cellID = detId.getFieldValue("cell");
+                int moduleID = detId.getFieldValue("module_position");
+                int combinedID = cellID*10+moduleID;
+                //std::cout << " [ CylinderEcalVetoProcessor ] : rawid " << rawID << " layer " << layer << " cellID " << cellID << " moduleID " << moduleID << " combineid " << combinedID << std::endl;
+                std::pair<double,double> hitPosition = hexReadout_->getCellCenterAbsolute(combinedID);
+                //std::cout << " [ CylinderEcalVetoProcessor ] : hit position: " << hitPosition.first << " " << hitPosition.second << std::endl;
+
+                std::vector<double> relativePosition(2,0.);
+                std::vector<double> rayPosition(2,0.);
+                rayPosition[0] = position[0]+pvec[0]/pvec[2]*(layer_z[layer]-position[2]);
+                rayPosition[1] = position[1]+pvec[1]/pvec[2]*(layer_z[layer]-position[2]);
+                Debug("ray position: ",rayPosition);
+                relativePosition[0] = hitPosition.first - rayPosition[0];
+                relativePosition[1] = hitPosition.second - rayPosition[1];
+                Debug("relative position: ",relativePosition);
+
+
+                double r = sqrt(pow(relativePosition[0],2)+pow(relativePosition[1],2));
+
+                Debug("grouping hits by radius");
+                if( r < moliereR_ ){
+                    cylinder_0_1[layer]+=hit->getEnergy();
+                }else if( r < 3*moliereR_ ){
+                    cylinder_1_3[layer]+=hit->getEnergy();
+                }else if( r < 5*moliereR_ ){
+                    cylinder_3_5[layer]+=hit->getEnergy();
+                }else{
+                    cylinder_5[layer]+=hit->getEnergy();
+                }
+
+            }// end loop over hits
+
+            Debug("grouping layer 0");
+            //result_.cylinder_0_1_layer_0_0.back() = cylinder_0_1[0];
+            //result_.cylinder_1_3_layer_0_0.back() = cylinder_1_3[0];
+            //result_.cylinder_3_5_layer_0_0.back() = cylinder_3_5[0];
+            //result_.cylinder_5_layer_0_0.back()   = cylinder_5[0];
+            Debug("grouping layer 1-2");
+            for( int i = 1 ; i < 3 ; i++ ){
+                //result_.cylinder_0_1_layer_1_2.back() += cylinder_0_1[i];
+                //result_.cylinder_1_3_layer_1_2.back() += cylinder_1_3[i];
+                //result_.cylinder_3_5_layer_1_2.back() += cylinder_3_5[i];
+                //result_.cylinder_5_layer_1_2.back()   += cylinder_5[i];
+            }
+            Debug("grouping layer 3-6");
+            for( int i = 3 ; i < 7 ; i++ ){
+                //result_.cylinder_0_1_layer_3_6.back() += cylinder_0_1[i];
+                //result_.cylinder_1_3_layer_3_6.back() += cylinder_1_3[i];
+                //result_.cylinder_3_5_layer_3_6.back() += cylinder_3_5[i];
+                //result_.cylinder_5_layer_3_6.back()   += cylinder_5[i];
+            }
+            Debug("grouping layer 7-14");
+             for( int i = 7 ; i < 15 ; i++ ){
+                //result_.cylinder_0_1_layer_7_14.back() += cylinder_0_1[i];
+                //result_.cylinder_1_3_layer_7_14.back() += cylinder_1_3[i];
+                //result_.cylinder_3_5_layer_7_14.back() += cylinder_3_5[i];
+                //result_.cylinder_5_layer_7_14.back()   += cylinder_5[i];
+            }
+            Debug("grouping layer 15 and beyond");
+                //std::cout << "cylinder sizes: " << cylinder_0_1.size() << " " << cylinder_1_3.size() << " " << cylinder_3_5.size() << " " << cylinder_5.size() << std::endl;
+            for( int i = 15 ; i < 33 ; i++ ){
+                 //result_.cylinder_0_1_layer_15.back() += cylinder_0_1[i];
+                //result_.cylinder_1_3_layer_15.back() += cylinder_1_3[i];
+                //result_.cylinder_3_5_layer_15.back() += cylinder_3_5[i];
+                //result_.cylinder_5_layer_15.back()   += cylinder_5[i];
+            }
+            Debug("done grouping by layer");
+        }
+
+        Debug("done, adding information to event");
+       // event.addToCollection("CylinderEcalVetoProcessor", //result_);
+    }
+}
+
+DECLARE_PRODUCER_NS(ldmx, CylinderEcalVetoProcessor);
